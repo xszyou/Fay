8a4beb9d7fe290f195d852984a868ad2a22a315e
xszyou <xszyou@163.com>
2023-08-24 09:59:10 +0800

    Update nlp_ChatGLM2.py
    修正查库顺序

--- ai_module/nlp_ChatGLM2.py
+++ ai_module/nlp_ChatGLM2.py
@@ -1,26 +1,26 @@
 import json
 import requests
 from core.content_db import Content_Db
-content_db = Content_Db()
-list = content_db.get_list('all','desc',10)
-answer_info = dict()
-chat_list = []
-for val in list:
-    answer_info = dict()
-    if val[0] == "member":
-        answer_info["role"] = "user"
-        answer_info["content"] = val[2]
-    elif val[0] == "fay":
-        answer_info["role"] = "bot"
-        answer_info["content"] = val[2]
-    chat_list.append(answer_info)
+
 
 def question(cont):
-    print(chat_list)
+    content_db = Content_Db()
+    list = content_db.get_list('all','desc',10)
+    answer_info = dict()
+    chat_list = []
+    for val in list:
+        answer_info = dict()
+        if val[0] == "member":
+            answer_info["role"] = "user"
+            answer_info["content"] = val[2]
+        elif val[0] == "fay":
+            answer_info["role"] = "bot"
+            answer_info["content"] = val[2]
+        chat_list.append(answer_info)
     content = {
         "prompt":"请简单回复我。" +  cont,
         "history":chat_list}
-    url = "http://192.168.1.23:8000"
+    url = "http://127.0.0.1:8000"
     req = json.dumps(content)
     headers = {'content-type': 'application/json'}
     r = requests.post(url, headers=headers, data=req)
