894a9b6ea4bf1843d1bf2d1986c45d01ce419c4b
xszyou <xszyou@163.com>
2023-09-18 10:17:31 +0800

    提交Fay AI Agent版计划
    提交Fay AI Agent版计划

--- core/interact.py
+++
@@ -1,6 +0,0 @@
-class Interact:
-
-    def __init__(self, interleaver: str, interact_type: int, data: dict):
-        self.interleaver = interleaver
-        self.interact_type = interact_type
-        self.data = data
