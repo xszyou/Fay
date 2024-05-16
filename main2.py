496a59c6d2733b5d4d4304d795a06f8481e7d83a
yanyuxiyangzk@126.com <yanyuxiyangzk@126.com>
2024-05-16 18:53:57 +0800

    fay对接metahuman

---
+++ main2.py
@@ -0,0 +1,80 @@
+import os
+import sys
+from io import BytesIO
+
+# from PyQt5 import QtGui
+# from PyQt5.QtWidgets import QApplication
+
+from ai_module import ali_nls
+from ai_module import nlp_langchain
+from core import wsa_server
+from gui import flask_server
+# from gui.window import MainWindow
+from utils import config_util
+from scheduler.thread_manager import MyThread
+from core import content_db
+import sys
+sys.setrecursionlimit(sys.getrecursionlimit() * 5)
+import hashlib
+import os
+import time
+
+
+def __clear_samples():
+    if not os.path.exists("./samples"):
+        os.mkdir("./samples")
+    for file_name in os.listdir('./samples'):
+        if file_name.startswith('sample-'):
+            os.remove('./samples/' + file_name)
+
+
+def __clear_songs():
+    if not os.path.exists("./songs"):
+        os.mkdir("./songs")
+    for file_name in os.listdir('./songs'):
+        if file_name.endswith('.mp3'):
+            os.remove('./songs/' + file_name)
+
+def __clear_logs():
+    if not os.path.exists("./logs"):
+        os.mkdir("./logs")
+    for file_name in os.listdir('./logs'):
+        if file_name.endswith('.log'):
+            os.remove('./logs/' + file_name)
+           
+
+
+if __name__ == '__main__':
+    __clear_samples()
+    __clear_songs()
+    __clear_logs()
+    config_util.load_config()
+    contentdb = content_db.new_instance()
+    contentdb.init_db()     
+    ws_server = wsa_server.new_instance(port=10002)
+    ws_server.start_server()
+    web_ws_server = wsa_server.new_web_instance(port=10003)
+    web_ws_server.start_server()
+    #Edit by xszyou in 20230516:增加本地asr后，aliyun调成可选配置
+    if config_util.ASR_mode == "ali":
+        ali_nls.start()
+    flask_server.start() 
+    if config_util.key_chat_module == 'langchain':
+        nlp_langchain.save_all()
+    # app = QApplication(sys.argv)
+    # app.setWindowIcon(QtGui.QIcon('icon.png'))
+    # win = MainWindow()
+    # time.sleep(1)
+    # win.show()
+    # app.exit(app.exec_())
+    from threading import Thread,Event
+    def run_server():
+        print("程序已经运行，访问路径是127.0.0.1:5000：")
+        while True:
+            user_input=input("请输入命令")
+            if user_input == "quit":
+                break
+            else:
+                print("执行命令："+user_input)
+    Thread(target=run_server, args=[]).start()
+    
